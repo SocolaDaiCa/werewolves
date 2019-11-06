7141b23ccdad327518542ceddb9926fb55c1f7bb
Socola Đại Ca <tokentien@gmail.com>
2019-11-07 05:21:42 +0700

    Initial commit

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<style>
+		body{
+			background: blue;
+		}
+		img {
+			background: green;
+		}
+	</style>
+</head>
+<body>
+	<img src="dx-apprentice-seer_400x400.png" alt="">
+</body>
+</html>
